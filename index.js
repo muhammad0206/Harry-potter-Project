660eab685d64c3cfb31e0db0ed4bcd9c2dc22c3b
Kelveen Raj <kelveenrajv@gmail.com>
2025-07-27 11:13:57 +0800

    fix

--- index.js
+++ index.js
@@ -2,62 +2,64 @@ const HpDiv = document.getElementById("hp");
 // const query1 = document.getElementById("search1"); was use to search name.
 const query2 = document.getElementById("search2");
 const query3 = document.getElementById("search3");
-const API_URL = "https://hp-api.onrender.com/api/characters";
+
+const GET_ALL_API_URL = "https://hp-api.onrender.com/api/characters";
+const GET_CHARACTER_API_URL = "https://hp-api.onrender.com/api/character";
+
 const btn = document.getElementById("btn");
 let bdy = document.getElementById("body");
 
-const displayHp = (data) =>{
-    let hpName;
-    let hpId;
-    let hpImg;
-    let HpActor;
-    let alt_name;
-    let alt_name_2;
-    let home;
-    let wood;
-    let core;
-    let length;
-    let birth;
+const selectOptions = document.getElementById("search3");
 
+const displayHp = (data) => {
+  const { name, id, image, actor, alternate_names, house, wand, dateOfBirth } =
+    data;
 
-    for(let i = 0; i < data.length; i++){
-        if(data[i].actor.toLowerCase() == query2.value.toLowerCase().trim() || data[i].name.toLowerCase() == query3.value.toLowerCase().trim()){
-            hpName = data[i].name;
-            hpId = data[i].id;
-            hpImg = data[i].image;
-            HpActor = data[i].actor;
-            alt_name = data[i].alternate_names[0];
-            alt_name_2 = data[i].alternate_names[1];
-            home = data[i].house;
-            wood = data[i].wand.wood;
-            core = data[i].wand.core;
-            length = data[i].wand.length;
-            birth = data[i].dateOfBirth;
-            break;
-        }
-    }
-    if(birth === undefined || birth === null ){
-        birth = "not mentioned"
-    }
+  let hpName;
+  let hpId;
+  let hpImg;
+  let HpActor;
+  let alt_name;
+  let alt_name_2;
+  let home;
+  let wood;
+  let core;
+  let length;
+  let birth;
 
-    if(wood === "" || wood === undefined || wood === null){
-        wood = "P&C"
-    }
+  hpName = name;
+  hpId = id;
+  hpImg = image;
+  HpActor = actor;
+  alt_name = alternate_names[0];
+  alt_name_2 = alternate_names[1];
+  home = house;
+  wood = wand.wood;
+  core = wand.core;
+  length = wand.length;
+  birth = dateOfBirth;
 
-    if(core === "" || core === undefined || core === null){
-        core = "P&C"
-    }
+  if (birth === undefined || birth === null) {
+    birth = "not mentioned";
+  }
 
-    if(length === "" || length === undefined || length === null){
-        length = "P&C"
-    }
+  if (wood === "" || wood === undefined || wood === null) {
+    wood = "P&C";
+  }
 
-    if(query3.value === "select"){
-        alert("please select name");
-    }
+  if (core === "" || core === undefined || core === null) {
+    core = "P&C";
+  }
 
-    hpHTML =
-     `
+  if (length === "" || length === undefined || length === null) {
+    length = "P&C";
+  }
+
+  if (query3.value === "select") {
+    alert("please select name");
+  }
+
+  hpHTML = `
      <div class="demo">
         <div class="info">
             <h1 style="font-size: 20px;">${hpName}</h1>
@@ -104,36 +106,51 @@ const displayHp = (data) =>{
         </table>
      </div>
     `;
-    if(hpImg === ""){
-       HpDiv.innerHTML = "No picture found"; 
-    }else{
-        HpDiv.innerHTML = hpHTML;
+  if (hpImg === "") {
+    HpDiv.innerHTML = "No picture found";
+  } else {
+    HpDiv.innerHTML = hpHTML;
+  }
+
+  window.addEventListener("scroll", () => {
+    const wand = document.getElementById("wand");
+    const wandPosition = wand.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (wandPosition < screenHeight) {
+      wand.classList.add("animate");
     }
+  });
+};
 
-    window.addEventListener("scroll", ()=>{
-        const wand = document.getElementById("wand");
-        const wandPosition = wand.getBoundingClientRect().top;
-        const screenHeight = window.innerHeight;
+const getHp = async (selectValue) => {
+  const response = await fetch(`${GET_CHARACTER_API_URL}/${selectValue}`);
+  const data = await response.json();
+  displayHp(data[0]);
+};
 
-        if(wandPosition < screenHeight){
-            wand.classList.add("animate");
-        }
-    });
-
-}
-
-
-const getHp = async () =>{
-    const response = await fetch(API_URL);
-    const Status = response.status;
+const getAlLCharacters = async () => {
+  try {
+    const response = await fetch(GET_ALL_API_URL);
     const data = await response.json();
-    displayHp(data);
-}
 
+    data.forEach((character, index) => {
+      const option = document.createElement("option");
+      option.value = character.id;
+      option.textContent = character.name;
+      selectOptions.appendChild(option);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-
-btn.addEventListener("click",() =>{
-    getHp();
+btn.addEventListener("click", () => {
+  const selectValue = selectOptions.value;
+  getHp(selectValue);
 });
 
-
+document.addEventListener("DOMContentLoaded", () => {
+  // after page load
+  getAlLCharacters();
+});
